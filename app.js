f7c087633259e4e0ec6132e233d15ad9ef9f82de
Kuldeep7devi <kushallaxkar9@gmail.com>
2026-04-15 15:05:56 +0530

    Origin change

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors({
-  origin: "http://localhost:8080",
+  origin: "https://bravima.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
