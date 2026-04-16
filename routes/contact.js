8b16c44769de4e451a53ae71b04de2764f6da434
Kuldeep7devi <kushallaxkar9@gmail.com>
2026-04-16 19:54:25 +0530

    Some changes

--- routes/contact.js
+++ routes/contact.js
@@ -4,10 +4,38 @@ var nodemailer = require('nodemailer');
 const { default: Contact } = require('../Model/Contact');
 const contactTemplate = require('../Templete/contactTemplate ');
 
+router.get('/', async (req, res) => {
+    try {
+        const data = await Contact.find();
+        res.status(200).json({
+            message: "Successful",
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error to get"
+        })
+    }
+});
+
+router.delete('/delete-contact/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await Contact.findByIdAndDelete(id);
+        res.status(200).json({
+            message: "Successfully delete",
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error to delete"
+        })
+    }
+})
+
 router.post('/message', async (req, res) => {
     try {
         const { name, email, message } = req.body;
-        console.log(name, email, message)
 
         const newMessage = await Contact.create({
             name,
